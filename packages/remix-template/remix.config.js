e54a146332ca24e268d6d00f8481627d164e5763
Katsuyuki Omuro <komuro@fastly.com>
2022-12-06 14:58:09 +0900

    Add comment to config file default values

--- packages/remix-template/remix.config.js
+++ packages/remix-template/remix.config.js
@@ -3,6 +3,7 @@ module.exports = {
   serverBuildTarget: "fastly-compute-js",
   devServerBroadcastDelay: 10000, // Adjust this if this is too short
   ignoredRouteFiles: ["**/.*"],
+  // The following are the default values, feel free to uncomment and make adjustments as necessary
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
