f75ade6df44bf00f691a9e40a7605f414b021b72
Katsuyuki Omuro <komuro@fastly.com>
2023-02-20 16:10:47 +0900

    Updated start page created out of the template

--- packages/remix-template/app/routes/index.tsx
+++ packages/remix-template/app/routes/index.tsx
@@ -1,23 +1,31 @@
+import styles from "~/styles/index.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Congratulations!</h1>
+      <p>
+        You’ve successfully created your <a target="_blank" href="https://remix.run/">Remix</a> project for <a target="_blank" href="https://developer.fastly.com/learning/compute/javascript/">Fastly Compute@Edge</a>.
+      </p>
+      <p>
+        If you already have a Remix application that you'd like to move to Compute@Edge, see
+        our <a target="_blank" href="https://github.com/fastly/remix-compute-js/blob/main/MIGRATING.md">Migration Guide</a>.
+      </p>
+      <p>
+        To learn more about Remix, visit some pages provided by Remix:
+      </p>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
+          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
             15m Quickstart Blog Tutorial
           </a>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
+          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
             Deep Dive Jokes App Tutorial
           </a>
         </li>
@@ -27,6 +35,18 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <p>
+        The Compute@Edge Adapter for Remix is provided as part of <a target="_blank" href="https://developer.fastly.com/labs/">Fastly Labs</a>&mdash;where Edge computing meets the cutting edge.
+        Visit Fastly Labs for other exciting projects we provide under this program.
+      </p>
+      <p>
+        Check out Fastly’s <a target="_blank" href="https://www.fastly.com/fast-forward">Fast Forward program</a> to learn more about our broad-reaching
+        programs designed to empower and support open source projects, nonprofit organizations, and developers in their endeavors to build great things
+        with unmatched ease, performance, and security.
+      </p>
+      <p>
+        Enjoy!
+      </p>
     </div>
   );
 }
